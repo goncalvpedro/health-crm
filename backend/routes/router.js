3f6348b5565584c8f6e386ef92f7fc1a6ecb1275
Pedro Gonçalves <153463894+goncalvpedro@users.noreply.github.com>
2025-06-07 18:04:56 -0300

    Fix auth middleware and controllers

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const userRoutes = require("./userRoutes");
+const patientRoutes = require("./patientRoutes");
+const { ensureAuth } = require("../middleware/auth-middleware");
 
 const router = express.Router();
+
+router.use(userRoutes);
+router.use(patientRoutes);
+
 router.get("/auth/test", ensureAuth, (req, res) => res.json({ message: "ok" }));
-
-
-
-
-
-
 
 module.exports = router;
